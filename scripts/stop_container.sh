5f8404bab31b80dbefaf757783bda67080294172
arkumbar3124 <arkumbar12@gmail.com>
2025-03-30 12:13:25 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-sudo sh -c 'docker rm -f $(docker ps -q | head -n 1)'
+#sudo sh -c 'docker rm -f $(docker ps -q | head -n 1)'
+sudo docker ps -q | xargs -r sudo docker rm -f
